5b331c20a8ef522d4354ac48abfc014872a8db74
ADAydin1905 <aydindevrim36@googlemail.com>
2026-06-15 10:18:08 +0200

    Admin-Panel: Update tarife.js, rabatte.js (15.6.2026, 10:18:07)

--- tarife.js
+++ tarife.js
@@ -1,6 +1,6 @@
 /*
  * tarife.js  -  Tarifdaten fuer den Business Mobil Rechner (v2.0)
- * Automatisch erzeugt vom Admin-Panel am 15.6.2026, 10:15:43
+ * Automatisch erzeugt vom Admin-Panel am 15.6.2026, 10:18:03
  * Einzige Tarifdaten-Quelle (per <script> geladen). Vom Admin-Panel erzeugt.
  */
 window.TARIFE_DATA = {
